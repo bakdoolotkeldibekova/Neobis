cfd8ff073360c99079dc985579666bec7eaf1975
bakdoolotkeldibekova <54860550+bakdoolotkeldibekova@users.noreply.github.com>
2020-02-15 09:18:08 +0600

    Add files via upload

---
+++ Week5/src/main/java/Cpu.java
@@ -0,0 +1,43 @@
+public class Cpu {
+    private int id;
+    private String model;
+    private int nucleus;
+    private double frequency;
+
+    //constructors
+    public Cpu(){};
+    public Cpu(int id, String model, int nucleus, double frequency){
+        this.id = id;
+        this.model = model;
+        this.nucleus = nucleus;
+        this.frequency = frequency;
+    }
+
+    //getters&setters
+    public String getModel() {
+        return model;
+    }
+    public int getNucleus() {
+        return nucleus;
+    }
+    public double getFrequency() {
+        return frequency;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setNucleus(int nucleus) {
+        this.nucleus = nucleus;
+    }
+    public void setFrequency(double frequency) {
+        this.frequency = frequency;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ Week5/src/main/java/Main.java
@@ -0,0 +1,76 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import java.util.List;
+
+public class Main {
+    private static SessionFactory sessionFactory;
+
+    public static void main(String[] args) {
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+        Main cpu = new Main();
+
+        //Adding Cpu to the DB:
+        cpu.addCpu(1, "intel", 2, 16000);
+        cpu.addCpu(2, "corei 5", 4, 19000);
+
+        System.out.println("List of Cpu: ");
+        List cpuList = cpu.listCpu();
+        for (Object c: cpuList) {
+            System.out.println(c);
+        }
+
+        System.out.println("\n\nUpdating and Removing Cpu");
+        cpu.updateCpu(3, 50000);
+        cpu.removeCpu(1);
+
+    }
+
+    public void addCpu(int id, String model, int nucleus, double frequency) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        transaction = session.beginTransaction();
+        Cpu cpu1 = new Cpu(id, model, nucleus, frequency);
+        session.save(cpu1);
+        transaction.commit();
+        session.close();
+    }
+
+    public List listCpu() {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        transaction = session.beginTransaction();
+        List list = session.createQuery("from Cpu").list();
+
+        transaction.commit();
+        session.close();
+        return list;
+    }
+
+    public void updateCpu(int Id, double frequency) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        transaction = session.beginTransaction();
+        Cpu cpu = (Cpu) session.get(Cpu.class, Id);
+        cpu.setFrequency(frequency);
+        session.update(cpu);
+        transaction.commit();
+        session.close();
+    }
+
+    public void removeCpu(int Id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        transaction = session.beginTransaction();
+        Cpu cpu = (Cpu) session.get(Cpu.class, Id);
+        session.delete(cpu);
+        transaction.commit();
+        session.close();
+    }
+
+}
