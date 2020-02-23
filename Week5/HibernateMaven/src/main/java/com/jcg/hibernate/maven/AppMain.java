12ed917d9e9a5f62a9da5c68a6e7e9f005a85b04
bakdoolotkeldibekova <54860550+bakdoolotkeldibekova@users.noreply.github.com>
2020-02-23 10:15:18 +0600

    Update AppMain.java

--- Week5/HibernateMaven/src/main/java/com/jcg/hibernate/maven/AppMain.java
+++ Week5/HibernateMaven/src/main/java/com/jcg/hibernate/maven/AppMain.java
@@ -1,56 +1,71 @@
-package com.jcg.hibernate.maven;
-
+package Entity;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
+import java.util.List;
 
 public class AppMain {
+	private static SessionFactory sessionFactory;
 
-	static Cpu cpuObj;
-	static Session sessionObj;
-	static SessionFactory sessionFactoryObj;
+	public static void main(String[] args) {
+		sessionFactory = new Configuration().configure().buildSessionFactory();
+		AppMain runner = new AppMain();
 
-	private static SessionFactory buildSessionFactory() {
-		// Creating Configuration Instance & Passing Hibernate Configuration File
-		Configuration configObj = new Configuration();
-		configObj.configure("hibernate.cfg.xml");
+		//runner.addCpu("corrrr", 45, 258);
+		//runner.updateEmployee(2, 50000);
+		//runner.removeCpu(4);
 
-		// Since Hibernate Version 4.x, ServiceRegistry Is Being Used
-		ServiceRegistry serviceRegistryObj = new StandardServiceRegistryBuilder().applySettings(configObj.getProperties()).build(); 
-
-		// Creating Hibernate SessionFactory Instance
-		sessionFactoryObj = configObj.buildSessionFactory(serviceRegistryObj);
-		return sessionFactoryObj;
-	}
-
-	public static void main(String[] args){
-		System.out.println(".......Hibernate Maven Example.......\n");
-		try {
-			sessionObj = buildSessionFactory().openSession();
-			sessionObj.beginTransaction();
-
-			for(int i = 5; i < 10; i++) {
-				cpuObj = new Cpu(i, "corei" + i, i%2==0 ? 4 : 2, 16000+i);
-
-				sessionObj.save(cpuObj);
-			}
-			System.out.println("\n.......Records Saved Successfully To The Database.......\n");
-
-			// Committing The Transactions To The Database
-			sessionObj.getTransaction().commit();
-		} catch(Exception sqlException) {
-			if(null != sessionObj.getTransaction()) {
-				System.out.println("\n.......Transaction Is Being Rolled Back.......");
-				sessionObj.getTransaction().rollback();
-			}
-			sqlException.printStackTrace();
-			System.out.println(sqlException.getMessage());
-		} finally {
-			if(sessionObj != null) {
-				sessionObj.close();
-			}
+		List cpuList = runner.listCpu();
+		for (Object cpu: cpuList) {
+			System.out.println(cpu);
 		}
 	}
+
+	public void addCpu(String model, int n, double f) {
+		Session session = sessionFactory.openSession();
+		Transaction transaction = null;
+
+		transaction = session.beginTransaction();
+		Cpu cpu = new Cpu(model ,n, f);
+		session.save(cpu);
+		transaction.commit();
+		session.close();
+	}
+
+	public List listCpu() {
+		Session session = this.sessionFactory.openSession();
+		Transaction transaction = null;
+
+		transaction = session.beginTransaction();
+		List developers = session.createQuery("FROM Cpu").list();
+
+		transaction.commit();
+		session.close();
+		return developers;
+	}
+
+    public void updateEmployee(int Id, int nucleus) {
+        Session session = this.sessionFactory.openSession();
+        Transaction transaction = null;
+
+        transaction = session.beginTransaction();
+        Cpu cpu = (Cpu) session.get(Cpu.class, Id);
+        cpu.setNucleus(nucleus);
+        session.update(cpu);
+        transaction.commit();
+        session.close();
+    }
+
+    public void removeCpu(int Id) {
+        Session session = this.sessionFactory.openSession();
+        Transaction transaction = null;
+
+        transaction = session.beginTransaction();
+			Cpu cpu = (Cpu) session.get(Cpu.class,Id);
+        session.delete(cpu);
+        transaction.commit();
+        session.close();
+    }
+
 }
